6536e42afad12254f462630c1a25b7671be8e5ad
misterli <misterli@tencent.com>
2021-08-08 18:30:07 +0800

    update

--- 001_两数之和.go
+++ 001_两数之和.go
@@ -1,3 +1,4 @@
+// 双循环
 func twoSum(nums []int, target int) []int {
     var index1 int
     for i, v := range nums {
@@ -7,6 +8,20 @@ func twoSum(nums []int, target int) []int {
             if nums[k] == val {
                 return []int{index1, k}
             }
+        }
+    }
+    return []int{}
+}
+
+// 使用map缓存
+func twoSum(nums []int, target int) []int {
+    cache := make(map[int]int)
+    for i, v := range nums {
+        another := target - v
+        if ii, ok := cache[another]; ok {
+            return []int{i, ii}
+        } else {
+            cache[v] = i
         }
     }
     return []int{}
