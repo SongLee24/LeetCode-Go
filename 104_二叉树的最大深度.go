b71ee4ff23e8fba30c07b26afeb45d9f54a8f0c4
SongLee24 <lisong.shine@qq.com>
2022-04-24 11:14:43 +0800

    update

--- 104_二叉树的最大深度.go
+++ 104_二叉树的最大深度.go
@@ -4,11 +4,11 @@ package main
  * Definition for a binary tree node.
  */
 
-type TreeNode struct {
-	Val   int
-	Left  *TreeNode
-	Right *TreeNode
-}
+//type TreeNode struct {
+//	Val   int
+//	Left  *TreeNode
+//	Right *TreeNode
+//}
 
 func maxDepth(root *TreeNode) int {
 	if root == nil {
--- 236_二叉树的最近公共祖先.go
+++ 236_二叉树的最近公共祖先.go
@@ -3,11 +3,11 @@ package main
 /**
  * Definition for a binary tree node.
  */
-type TreeNode struct {
-	Val   int
-	Left  *TreeNode
-	Right *TreeNode
-}
+//type TreeNode struct {
+//	Val   int
+//	Left  *TreeNode
+//	Right *TreeNode
+//}
 
 func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
 	if root == nil {
